dcd07bff577a4798a7f63e9c78b90c05f1b44558
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-12-12 13:24:46 +0100

    end form milestone 1, start handleSubmit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,82 @@
+import { useState } from "react"
+
 function App() {
+
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [specialization, setSpecialization] = useState("");
+  const [yearExp, setYearExp] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name.trim() || !username.trim() || !password.trim() || !specialization.trim() || !yearExp.trim() || !description.trim()) {
+
+    }
+
+
+  }
 
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <h1>hello</h1>
+            <h1>Form iscrizione Web Developer</h1>
+            <form onSubmit={handleSubmit}>
+              <label>
+                <span>Nome completo</span>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)} />
+              </label>
+              <label>
+                <span>Username</span>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)} />
+              </label>
+              <label>
+                <span>Password</span>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)} />
+              </label>
+              <label>
+                <span>Specializzazione</span>
+                <select
+                  value={specialization}
+                  onChange={(e) => setSpecialization(e.target.value)}
+                >
+                  <option value="">Seleziona una specializzazione</option>
+                  <option value="Full Stack">Full Stack</option>
+                  <option value="Frontend">Frontend</option>
+                  <option value="Backend">Backend</option>
+                </select>
+              </label>
+              <label>
+                <span>Anni di esperienza</span>
+                <input
+                  type="number"
+                  value={yearExp}
+                  onChange={(e) => setYearExp(e.target.value)} />
+              </label>
+              <label>
+                <span>Descrizione</span>
+                <textarea
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                >
+                </textarea>
+              </label>
+            </form>
           </div>
+
         </div>
       </div>
     </>
