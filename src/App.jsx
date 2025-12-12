8f4aed3f364dd50d02c3911de9e2cab0a0499591
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-12-12 13:40:30 +0100

    fix with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -24,58 +24,76 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="row">
+        <div className="row ">
           <div className="col-12">
             <h1>Form iscrizione Web Developer</h1>
             <form onSubmit={handleSubmit}>
-              <label>
-                <span>Nome completo</span>
-                <input
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)} />
-              </label>
-              <label>
-                <span>Username</span>
-                <input
-                  type="text"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)} />
-              </label>
-              <label>
-                <span>Password</span>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)} />
-              </label>
-              <label>
-                <span>Specializzazione</span>
-                <select
-                  value={specialization}
-                  onChange={(e) => setSpecialization(e.target.value)}
-                >
-                  <option value="">Seleziona una specializzazione</option>
-                  <option value="Full Stack">Full Stack</option>
-                  <option value="Frontend">Frontend</option>
-                  <option value="Backend">Backend</option>
-                </select>
-              </label>
-              <label>
-                <span>Anni di esperienza</span>
-                <input
-                  type="number"
-                  value={yearExp}
-                  onChange={(e) => setYearExp(e.target.value)} />
-              </label>
-              <label>
-                <span>Descrizione</span>
-                <textarea
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                >
-                </textarea>
-              </label>
+              <div>
+                <label className="form-label">
+                  <span>Nome completo</span>
+                  <input
+                    className="form-control"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} />
+                </label>
+              </div>
+              <div>
+                <label className="form-label">
+                  <span>Username</span>
+                  <input
+                    className="form-control"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)} />
+                </label>
+              </div>
+              <div>
+                <label className="form-label">
+                  <span>Password</span>
+                  <input
+                    className="form-control"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+                </label>
+              </div>
+              <div>
+                <label className="form-label">
+                  <span>Specializzazione</span>
+                  <select
+                    className="form-select"
+                    value={specialization}
+                    onChange={(e) => setSpecialization(e.target.value)}
+                  >
+                    <option value="">Seleziona una specializzazione</option>
+                    <option value="Full Stack">Full Stack</option>
+                    <option value="Frontend">Frontend</option>
+                    <option value="Backend">Backend</option>
+                  </select>
+                </label>
+              </div>
+              <div>
+                <label className="form-label">
+                  <span>Anni di esperienza</span>
+                  <input
+                    className="form-control"
+                    type="number"
+                    value={yearExp}
+                    onChange={(e) => setYearExp(e.target.value)} />
+                </label>
+              </div>
+              <div>
+                <label className="form-label">
+                  <span>Descrizione</span>
+                  <textarea
+                    className="form-control"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  >
+                  </textarea>
+                </label>
+              </div>
               <button className="btn btn-primary">Iscriviti</button>
             </form>
           </div>
